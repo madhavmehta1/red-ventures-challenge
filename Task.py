4813c61cc61de09de728929b65ac7e9e7e183348
Madhav Mehta <madhavm16@tutanota.com>
2020-01-20 14:23:54 -0600

    add task info function

--- Task.py
+++ Task.py
@@ -3,23 +3,25 @@ class Task:
         self.description = description
         self.eta = eta
         self.robot_type = robot_type
-    
 
     def __eq__(self, other):
         return self.description == other.description
 
-
     def __hash__(self):
         return hash(self.description)
 
-
     def get_eta(self):
         return self.eta
-    
 
     def get_description(self):
         return self.description
 
-
     def get_robot_type(self):
         return self.robot_type
+
+    def get_task_info(self):
+        return {
+            "description": self.get_description(),
+            "eta": self.get_eta(),
+            "robot_type": self.get_robot_type()
+        }
