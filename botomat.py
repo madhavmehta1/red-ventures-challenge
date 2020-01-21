6c5227338d84be690a510636130ab2a95b4e27fc
Madhav Mehta <madhavm16@tutanota.com>
2020-01-21 01:33:21 -0600

    add custom task functionality and fix view tables

--- botomat.py
+++ botomat.py
@@ -1,5 +1,6 @@
 from random import choice
 import asyncio
+from prettytable import PrettyTable
 
 from Task import Task
 from RobotEntity import Robot
@@ -13,43 +14,64 @@ task_collection = db["tasklist"]
 def main():
     task_set = create_tasks()
     while True:
+        loop = asyncio.get_event_loop()
         print_menu()
         user_input = get_menu_selection()
         if user_input == "1":
             robot_set = create_robots()
             robot_set = assign_tasks(robot_set, task_set)
-            perform_tasks(robot_set)
+            perform_tasks(robot_set, loop)
             robot_list = convert_robot_data(robot_set)
             update_leaderboard(robot_list)
         elif user_input == "2":
             view_leaderboard(sort_leaderboard())
         elif user_input == "3":
             view_tasks()
+        elif user_input == "4":
+            task_set = create_custom_tasks(task_set)
         else:
+            loop.close()
             break
+
+
+def create_custom_tasks(task_set):
+    print("\nIn order to create a new custom task, you must enter the relevant details when prompted to. "
+          "Please keep in mind that you must enter a valid robot type.\nThese are valid input for robot type: "
+          "Unipedal, Bipedal, Quadrupedal, Arachnid, Radial, Aeronautical.\n"
+          "NOTE: these tasks may not be picked up by the assignment process and if they are not, new robots "
+          "must be created until they are picked up.\n")
+    while True:
+        description = input("Please enter a description of the task: ")
+        eta = float(input("Please enter an estimated time of completion in milliseconds: "))/float(1000)
+        robot_type = input("Please enter the type of robot allowed to complete this task: ")
+        new_task = Task(description, eta, robot_type)
+        task_set.add(new_task)
+        user_input = input("Would you like to add more tasks? Type Y for yes and N for no.")
+        if user_input == "N":
+            break
+    return task_set
 
 
 def view_tasks():
     all_tasks = task_collection.find()
-    print("|           Description           |   ETA(seconds)   |   Robot Type   |")
-    print("|---------------------------------------------------------------------|")
-    output = ""
-    for task in all_tasks:
-        output = output + "|" + task["description"] + "           |" + str(task["eta"]) + \
-                 "           |" + task["robot_type"] + "           |\n"
-        output += "|------------------------------------------------------------------------------------------------|\n"
-    print(output)
+    table = PrettyTable(["Description", "ETA(milliseconds)", "Robot Type"])
+    table.align["Description"] = "l"
+    for record in all_tasks:
+        table.add_row([record["description"], record["eta"], record["robot_type"]])
+    print(table)
 
 
 def view_leaderboard(leaderboard):
-    print("|   Robot Name   |   Robot Type   |   Task Count   |")
-    print("|----------------|----------------|----------------|")
-    output = ""
+    table = PrettyTable(["Robot Name", "Robot Type", "Task Count", "Completed Tasks"])
+    table.align["Completed Tasks"] = "l"
+    table.align["Robot Type"] = "l"
+    table.align["Robot Name"] = "l"
     for record in leaderboard:
-        output = output + "|    " + record["name"] + "         |  " + record["type"] + "  |      " \
-                 + str(record["task_count"]) + "         |\n"
-        output += "|----------------|----------------|----------------|\n"
-    print(output)
+        completed_tasks = set()
+        for task in record["completed_tasks"]:
+            completed_tasks.add(task["description"])
+        table.add_row([record["name"], record["type"], record["task_count"], completed_tasks])
+    print(table)
 
 
 def sort_leaderboard():
@@ -66,10 +88,8 @@ async def complete_tasks(robot):
     print(robot.get_robot_name() + " has completed all their tasks.")
 
 
-def perform_tasks(robot_set):
-    loop = asyncio.get_event_loop()
+def perform_tasks(robot_set, loop):
     loop.run_until_complete(asyncio.gather(*(complete_tasks(robot) for robot in robot_set)))
-    loop.close()
 
 
 def assign_tasks(robot_set, task_set):
@@ -110,7 +130,7 @@ def convert_robot_data(robot_set):
 
 def get_menu_selection():
     user_input = ""
-    while user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4":
+    while user_input != "1" and user_input != "2" and user_input != "3" and user_input != "4" and user_input != "5":
         user_input = input("Please enter one of the above menu options: ")
     return user_input
 
@@ -130,7 +150,8 @@ def print_menu():
     print("1. Add Robot(s)")
     print("2. View Leaderboard")
     print("3. View Completed Tasks")
-    print("4. Exit")
+    print("4. Add Custom Task(s)")
+    print("5. Exit")
 
 
 if __name__ == "__main__":
